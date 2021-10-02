8d06c028c321d8b29b4b50dfbf1d7aead31af84d
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-10-02 19:46:55 +0700

    add operation xor

---
+++ ops/xor.py
@@ -0,0 +1,6 @@
+def xor_ops(pixels1, pixels2):
+    new_pixels = []
+    for i in range(len(pixels1)):
+        pixel = int(pixels1[i] ^ pixels2[i])
+        new_pixels.append(pixel)
+    return new_pixels
