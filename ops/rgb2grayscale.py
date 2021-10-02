8a8be6fbb072a2b2a4b1ca6dfc122b12a4bb3fc8
Ahmad Alif Sofian <alif12sofian@gmail.com>
2021-10-02 19:10:55 +0700

    delete print in rgb2grayscale

--- ops/rgb2grayscale.py
+++ ops/rgb2grayscale.py
@@ -1,5 +1,4 @@
 def rgb2grayscale(pixels):
-    print(pixels[0][0])
     for index, pixel in enumerate(pixels):
         imgGray = 0.299 * pixels[index][0] + 0.587 * \
             pixels[index][1] + 0.1440 * pixels[index][2]
