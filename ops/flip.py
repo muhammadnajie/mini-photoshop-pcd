c12997568f36f065016821a6bdaa3063c8de60ab
Muhammad Najie <muhammadalnajie1@gmail.com>
2021-10-02 22:08:44 +0700

    vertical flip

--- ops/flip.py
+++ ops/flip.py
@@ -1,9 +1,17 @@
 from utils.utils import convert_1d_to_2d, flatten
 import numpy as np
 
-def vertical_flip():
-    # TODO: Vertical Flip
-    pass
+def vertical_flip(pixels, image_size):
+    pixels_2d = convert_1d_to_2d(pixels, image_size)
+    flipped_image = []
+    for i in range(image_size[1]):
+        flipped_image.append([0] * image_size[0])
+    K = image_size[1] - 1
+    for i in range(image_size[1]):
+        for j in range(image_size[0]):
+            flipped_image[K][j] = pixels_2d[i][j]
+        K -= 1
+    return flipped_image
 
 def horizontal_flip(pixels, image_size):
     """
@@ -14,9 +22,9 @@ def horizontal_flip(pixels, image_size):
     pixels_2d = convert_1d_to_2d(pixels, image_size)
     flipped_image = []
     W = image_size[0]
-    for i in range(len(pixels_2d)):
+    for i in range(image_size[1]):
         row = []
-        for j in range(len(pixels_2d[i])):
+        for j in range(image_size[0]):
             row.append(pixels_2d[i][W-j-1])
         flipped_image.append(row)
     return flipped_image
