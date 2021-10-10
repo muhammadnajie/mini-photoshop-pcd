1ffe02e5f0d47a8cbbf38906a02c2690552a09de
Nashrullah29 <muhammad.118140136@student.itera.ac.id>
2021-10-10 20:49:31 +0700

    add zoom in feature

--- ops/zooming.py
+++ ops/zooming.py
@@ -1,5 +1,5 @@
 import utils.utils as util
-
+from division import division
 
 def zoomout(pixels, image_size, x, y):
     new_pixels = util.convert_1d_to_2d(pixels, image_size)
@@ -21,4 +21,47 @@ def copy_column(pixels, x):
         for column in row:
             new_row.append([column for i in range(x)])
         new_pixels.append(new_row)
+    return new_pixels
+
+def zoomin(pixels, image_size, x, y):
+    new_pixels = util.convert_1d_to_2d(pixels, image_size)
+    new_pixels = add_row(new_pixels, y)
+    new_pixels = add_column(new_pixels, x)
+    new_pixels = division(new_pixels, x*y)
+    return new_pixels
+
+def add_row(pixels, y):
+    i = 0
+    sum_temp = 0
+    row_temp = []
+    new_pixels = []
+    width = len(pixels[0])
+    height = len(pixels)
+    for column in range(width):
+        for row in range(height):
+            sum_temp += pixels[row][column]
+            i += 1
+            if i == y:
+                row_temp.append(sum_temp)
+                sum_temp = 0
+                i = 0
+        new_pixels.append(row_temp.copy())
+        row_temp.clear()
+    return new_pixels
+
+def add_column(pixels, x):
+    i = 0
+    sum_temp = 0
+    row_temp = []
+    new_pixels = []
+    for row in pixels:
+        for pixel in row:
+            sum_temp += pixel
+            i += 1
+            if i == x:
+                row_temp.append(sum_temp)
+                sum_temp = 0
+                i = 0
+        new_pixels.append(row_temp.copy())
+        row_temp.clear()
     return new_pixels
